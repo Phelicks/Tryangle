48e7040b409fbb1c3af1051df96a30ac8b555e26
Felix <FeJa-92@myopera.com>
2013-04-16 20:23:13 +0200

    Added Vertex.java

--- Tryangle/src/com/januskopf/tryangle/entity/Triangle.java
+++ Tryangle/src/com/januskopf/tryangle/entity/Triangle.java
@@ -5,17 +5,20 @@ public class Triangle {
 	private float colorR;
 	private float colorG;
 	private float colorB;
-	private float xPos;
-	private float yPos;
 	private float length;
+	
+	private Vertex vertiecs[] = new Vertex[3];
 	
 	public Triangle(float xPos, float yPos, float length, float colorR, float colorG, float colorB){
 		this.colorR = colorR;
 		this.colorG = colorG;
 		this.colorB = colorB;
-		this.xPos= xPos;
-		this.yPos = yPos;
 		this.length = length;
+
+		vertiecs[0] = new Vertex(xPos, yPos);
+		vertiecs[1] = new Vertex(xPos, yPos);
+		vertiecs[2] = new Vertex(xPos, yPos);
+		
 	}
 	
 	public float getColorR() {
@@ -29,15 +32,7 @@ public class Triangle {
 	public float getColorB() {
 		return colorB;
 	}
-	
-	public float xPos() {
-		return xPos;
-	}
-	
-	public float yPos() {
-		return yPos;
-	}
-	
+		
 	public float length() {
 		return length;
 	}
