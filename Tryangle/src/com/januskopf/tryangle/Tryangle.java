478d1a0d93e781a8c3183a3e91f68852d60311e0
Felix <FeJa-92@myopera.com>
2013-04-18 10:22:33 +0200

    Test

---
+++ Tryangle/src/com/januskopf/tryangle/Tryangle.java
@@ -0,0 +1,112 @@
+package com.januskopf.tryangle;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.*;
+
+import com.januskopf.tryangle.entity.*;
+ 
+public class Tryangle implements Runnable{
+	
+	public final int WIDTH = 800;
+	public final int HEIGHT = 600;
+	public final int FPS = 60;
+	
+	private boolean running;
+	private final int xTriCount = 20;
+	private final int yTriCount = 26;
+	
+	private Triangle[][] triangleArray = new Triangle[yTriCount][xTriCount];
+	
+	public static void main(String[] args)	{
+		Tryangle t = new Tryangle();
+		t.start();
+	}
+	
+	public void start()	{
+		this.running = true;
+		new Thread(this).start();
+	}
+	public void stop()	{
+		this.running = false;
+	}
+	
+	public void initDisplay(){
+		try{
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.setResizable(false);
+			PixelFormat p = new PixelFormat().withSamples(8);
+			Display.create(p);
+		}
+		catch (LWJGLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void initOpenGL(){
+		GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, WIDTH, HEIGHT, 0, -1.0, 1.0);
+		System.out.println("You are running OpenGL version ["+GL11.glGetString(GL11.GL_VERSION)+"].");
+	}
+	
+	public void run(){
+		this.initDisplay();
+		this.initOpenGL();
+		this.createTriArray();
+		while(running){
+			if(Display.isCloseRequested()) stop();
+			
+			this.tick();
+			this.render();
+			Display.sync(FPS);
+			Display.update();
+		}
+	}
+	
+		
+	private void createTriArray() {
+		
+		float xPos = 0.0f;
+		float yPos = -50.0f; //-50
+		float l = 50.0f; //50
+		float h = ((float)Math.sqrt((l*l) - ((l/2)*(l/2))));
+		
+		float x = xPos;
+		float y = yPos;
+		
+		for(int j = 0; j < yTriCount; j++){
+			for(int i = 0; i < xTriCount; i++){
+				
+				float c = (float)(1.0 - Math.random()/2.0);
+				
+				if(i % 2 == 0)
+					//triangleArray[j][i] = new Triangle(x, y, l, 0.85f, 0.48f, 0.13f);
+					triangleArray[j][i] = new Triangle(x, y, l, 1.0f*c, 0.47f*c, 0.0f*c);
+				else
+					triangleArray[j][i] = new Triangle(x + h, y, l, 1.0f*c, 0.47f*c, 0.0f*c, true);
+				
+				x = x + h;
+			}
+			if(j%2 == 0)
+				x = xPos;
+			else
+				x = xPos -h;
+			y = y + l/2;
+		}
+		
+	}
+
+	public void render(){
+		for(int j = 0; j < yTriCount; j++){
+			for(int i = 0; i < xTriCount; i++){
+				triangleArray[j][i].render();
+			}
+		}
+	}
+	
+	public void tick(){
+		
+	}
+ 
+}
---
+++ Tryangle/src/com/januskopf/tryangle/entity/Triangle.java
@@ -0,0 +1,59 @@
+package com.januskopf.tryangle.entity;
+
+import org.lwjgl.opengl.GL11;
+
+public class Triangle {
+	
+	private float colorR;
+	private float colorG;
+	private float colorB;
+	private float length;
+	
+	private Vertex vertiecs[] = new Vertex[3];
+	
+	public Triangle(float xPos, float yPos, float length, float colorR, float colorG, float colorB){
+		this(xPos, yPos, length, colorR, colorG, colorB, false);
+	}
+
+	public Triangle(float xPos, float yPos, float length, float colorR, float colorG, float colorB, boolean left){
+		this.colorR = colorR;
+		this.colorG = colorG;
+		this.colorB = colorB;
+		this.length = length;
+
+		vertiecs[0] = new Vertex(xPos, yPos);
+		vertiecs[1] = new Vertex(xPos, yPos + length);
+		
+		if (!left)
+			vertiecs[2] = new Vertex(xPos+((float)Math.sqrt((length * length) - ((length/2)*(length/2)))), yPos + length/2f);
+		else
+			vertiecs[2] = new Vertex(xPos-((float)Math.sqrt((length * length) - ((length/2)*(length/2)))), yPos + length/2f);
+		
+	}
+	
+	public void render(){
+		GL11.glColor3f(colorR, colorG, colorB);
+		GL11.glBegin(GL11.GL_TRIANGLES);
+			GL11.glVertex2f(vertiecs[0].getxPos(), vertiecs[0].getyPos());
+			GL11.glVertex2f(vertiecs[1].getxPos(), vertiecs[1].getyPos());
+			GL11.glVertex2f(vertiecs[2].getxPos(), vertiecs[2].getyPos());
+		GL11.glEnd();		
+	}
+	
+	public float getColorR() {
+		return colorR;
+	}
+	
+	public float getColorG() {
+		return colorG;
+	}
+	
+	public float getColorB() {
+		return colorB;
+	}
+		
+	public float length() {
+		return length;
+	}
+	
+}
