d36b9748a8e1825daee29982e3467821a69fd0ca
Klotz <Klotz@192.168.1.2>
2014-02-05 19:50:02 +0100

    Round circles in WaterAnimation.

--- Tryangle/src/com/januskopf/tryangle/triangles/animations/WaterAnimation.java
+++ Tryangle/src/com/januskopf/tryangle/triangles/animations/WaterAnimation.java
@@ -27,7 +27,7 @@ public class WaterAnimation extends Animations{
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.triangles = triangles;
-		this.iRadius = 0;
+		this.iRadius = 1;
 		this.radius = radius;
 	}
 	
@@ -54,53 +54,113 @@ public class WaterAnimation extends Animations{
 		
 		int x0 = triangles.getIndexFromPos(xPos, yPos).x;
 		int y0 = triangles.getIndexFromPos(xPos, yPos).y;
-		int dx = (int) -iRadius;
-		int dy = (int) -iRadius;
+//		int dx = (int) -iRadius;
+//		int dy = (int) -iRadius;
 		int x = (int) iRadius;
 		int y = 0;
-		while(y < x){
+		float xDif = iRadius;
+		
+//		while(y < x){
+//			
+//			/*
+//			GL11.glColor3f(1, 0, 0);
+//			GL11.glBegin(GL11.GL_POINTS);
+//				GL11.glVertex2f(xPos + x , yPos + y);
+//				GL11.glVertex2f(xPos - x , yPos + y);
+//				GL11.glVertex2f(xPos + x , yPos - y);
+//				GL11.glVertex2f(xPos - x , yPos - y);
+//				
+//				GL11.glVertex2f(xPos + y , yPos + x);
+//				GL11.glVertex2f(xPos - y , yPos + x);
+//				GL11.glVertex2f(xPos + y , yPos - x);
+//				GL11.glVertex2f(xPos - y , yPos - x);
+//			GL11.glEnd();
+//			*/
+//			Triangle[] triangle = new Triangle[8];
+//			triangle[0]  = triangles.getTriangle(x0 + x, y0 + y);
+//			triangle[1]  = triangles.getTriangle(x0 + x, y0 - y);
+//			triangle[2]  = triangles.getTriangle(x0 - x, y0 + y);
+//			triangle[3]  = triangles.getTriangle(x0 - x, y0 - y);
+//			
+//			triangle[4] = triangles.getTriangle(x0 + y, y0 + x);
+//			triangle[5] = triangles.getTriangle(x0 + y, y0 - x);
+//			triangle[6] = triangles.getTriangle(x0 - y, y0 + x);
+//			triangle[7] = triangles.getTriangle(x0 - y, y0 - x);
+//			
+//			float c = 0.01f - (0.04f - (0.04f/iRadius));
+//			for(Triangle t : triangle){				
+//				if(t != null){
+//					t.addBottomLayerEffect(new ColorFlash(colorR+c, colorG+c, colorB+c, 40));
+//				}
+//			}
+//			
+//			dx = dx + 2*y + 1;
+//			y = y + 1;
+//			if(dx > 0){
+//				dx = dx - 2 * x + 2;
+//				xDif = xDif - 0.866f;
+//				x = Math.round(xDif);
+//				//x = x - 1;
+//			}
+//		}
+		
+		
+		float aDif = iRadius*0.6f;
+		int a = Math.round(aDif);
+		int b = (int)iRadius;
+		int dx = 0;
+		int dy = a;
+		float a2 = a*a; 
+		float b2 = b*b;
+		float err = b2-(2*b-1)*a2;
+		float e2;
+		
+		
+		while(dy >= 0){
 			
-			GL11.glColor3f(1, 0, 0);
-			GL11.glBegin(GL11.GL_POINTS);
-				GL11.glVertex2f(xPos + x , yPos + y);
-				GL11.glVertex2f(xPos - x , yPos + y);
-				GL11.glVertex2f(xPos + x , yPos - y);
-				GL11.glVertex2f(xPos - x , yPos - y);
-				
-				GL11.glVertex2f(xPos + y , yPos + x);
-				GL11.glVertex2f(xPos - y , yPos + x);
-				GL11.glVertex2f(xPos + y , yPos - x);
-				GL11.glVertex2f(xPos - y , yPos - x);
-			GL11.glEnd();
 			
-			Triangle[] triangle = new Triangle[8];
-			triangle[0]  = triangles.getTriangle(x0 + x, y0 + y);
-			triangle[1]  = triangles.getTriangle(x0 + x, y0 - y);
-			triangle[2]  = triangles.getTriangle(x0 - x, y0 + y);
-			triangle[3]  = triangles.getTriangle(x0 - x, y0 - y);
+//			GL11.glColor3f(1, 0, 0);
+//			GL11.glBegin(GL11.GL_POINTS);
+//				GL11.glVertex2f(xPos + dx , yPos + dy);
+//				GL11.glVertex2f(xPos - dx , yPos + dy);
+//				GL11.glVertex2f(xPos + dx , yPos - dy);
+//				GL11.glVertex2f(xPos - dx , yPos - dy);
+//				
+//			GL11.glEnd();
 			
-			triangle[4] = triangles.getTriangle(x0 + y, y0 + x);
-			triangle[5] = triangles.getTriangle(x0 + y, y0 - x);
-			triangle[6] = triangles.getTriangle(x0 - y, y0 + x);
-			triangle[7] = triangles.getTriangle(x0 - y, y0 - x);
+			Triangle[] triangle = new Triangle[6];
+			triangle[0]  = triangles.getTriangle(x0 + dx+1, y0 + dy+1);
+			triangle[1]  = triangles.getTriangle(x0 + dx+1, y0 - dy);
+			triangle[2]  = triangles.getTriangle(x0 - dx, y0 + dy+1);
+			triangle[3]  = triangles.getTriangle(x0 - dx, y0 - dy);
 			
-			float c = 0.01f - (0.04f - (0.04f/iRadius));
+			
+			
+			e2 = 2*err;
+			if (e2 < (2*dx+1)*b2){
+				dx++;
+				err += (2*dx+1)*b2;
+			}
+			if (e2 > -(2*dy+1)*a2){
+				dy--;
+				err -= (2*dy-1)*a2;
+			}
+//			while (dx++ < a){
+//				triangle[4] = triangles.getTriangle(x0 + dx, y0);
+//				triangle[5] = triangles.getTriangle(x0 - dx, y0);
+//			}
+			
+			float c = 0.08f - (0.04f - (0.04f/iRadius));
 			for(Triangle t : triangle){				
 				if(t != null){
 					t.addBottomLayerEffect(new ColorFlash(colorR+c, colorG+c, colorB+c, 40));
 				}
 			}
 			
-			dx = dx + 2*y + 1;
-			y = y + 1;
-			if(dx > 0){
-				dx = dx - 2 * x + 2;
-				x = x - 1;
-			}
 		}
 		
 		if(iRadius <= radius){
-			iRadius += 0.2f;
+			iRadius += 0.3f;
 		}
 		else{
 			iRadius = 50;
